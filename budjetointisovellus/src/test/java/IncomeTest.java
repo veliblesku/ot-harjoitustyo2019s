f4bfc4d535ec3f5d75558cb5702d1d38eb43e130
veliblesku <vili.stick@gmail.com>
2019-12-22 22:02:11 +0200

    final clean

--- budjetointisovellus/src/test/java/IncomeTest.java
+++ budjetointisovellus/src/test/java/IncomeTest.java
@@ -1,53 +1,54 @@
-///*
-// * To change this license header, choose License Headers in Project Properties.
-// * To change this template file, choose Tools | Templates
-// * and open the template in the editor.
-// */
-//
-//import budjetointisovellus.budjetointisovellus.domain.Budget;
-//import budjetointisovellus.budjetointisovellus.domain.Expenses;
-//import budjetointisovellus.budjetointisovellus.domain.Income;
-//import budjetointisovellus.budjetointisovellus.domain.User;
-//import org.junit.After;
-//import org.junit.AfterClass;
-//import org.junit.Before;
-//import org.junit.BeforeClass;
-//import org.junit.Test;
-//import static org.junit.Assert.*;
-//
-///**
-// *
-// * @author blesku
-// */
-//public class IncomeTest {
-//
-//    Income income = new Income();
-//
-//    @Before
-//    public void setUp() {
-//        income.setTotalIncome(12021.3);
-//    }
-//
-//    @Test
-//    public void budjettiOnAlustettuOikein() {
-//        assertEquals(12021.3, income.getTotalIncome(), 0.00001);
-//    }
-//
-//    @Test
-//    public void setIncomeToimii() {
-//        income.setTotalIncome(2000.0);
-//        assertEquals(2000.0, income.getTotalIncome(), 0.000012);
-//    }
-//
-//    @Test
-//    public void getIncomeToimii() {
-//        assertEquals(income.getTotalIncome(), income.getTotalIncome(), 0.000012);
-//    }
-//
-//    @Test
-//    public void getIncomeToStringToimii() {
-//
-//        assertEquals("Income(totalIncome=12021.3)", income.toString());
-//    }
-//
-//}
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import budjetointisovellus.domain.Expense;
+import budjetointisovellus.domain.Income;
+import budjetointisovellus.domain.User;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author blesku
+ */
+public class IncomeTest {
+
+    Income income = new Income();
+
+    @Before
+    public void setUp() {
+        income.setAmount(5);
+        income.setName("pullojen palautus");
+    }
+
+    @Test
+    public void budjettiOnAlustettuOikein() {
+        assertEquals(5.0, income.getAmount(), 0.00001);
+        assertEquals("pullojen palautus", income.getName());
+    }
+
+    @Test
+    public void setIncomeToimii() {
+        income.setAmount(2000.0);
+        assertEquals(2000.0, income.getAmount(), 0.000012);
+    }
+
+    @Test
+    public void getIncomeToimii() {
+        assertEquals(5.0, income.getAmount(), 0.000012);
+    }
+
+    @Test
+    public void getJaSetNameToimii() {
+        income.setName("kivisaksetpaperi");
+        assertEquals("kivisaksetpaperi", income.getName());
+    }
+
+}
--- budjetointisovellus/src/test/java/UserTest.java
+++ budjetointisovellus/src/test/java/UserTest.java
@@ -1,42 +1,66 @@
-///*
-// * To change this license header, choose License Headers in Project Properties.
-// * To change this template file, choose Tools | Templates
-// * and open the template in the editor.
-// */
-//
-//import budjetointisovellus.budjetointisovellus.domain.Budget;
-//import budjetointisovellus.budjetointisovellus.domain.Expenses;
-//import budjetointisovellus.budjetointisovellus.domain.Income;
-//import budjetointisovellus.budjetointisovellus.domain.User;
-//import org.junit.After;
-//import org.junit.AfterClass;
-//import org.junit.Before;
-//import org.junit.BeforeClass;
-//import org.junit.Test;
-//import static org.junit.Assert.*;
-//
-///**
-// *
-// * @author blesku
-// */
-//public class UserTest {
-//
-//    User user;
-//    Income income = new Income();
-//    Expenses expenses = new Expenses();
-//    Budget budget;
-//    
-//
-//    @Before
-//    public void setUp() {
-//        this.income.setTotalIncome(12123.4);
-//        expenses.setTotalExpenses(11341.3);
-//        this.user = new User("Jani", "salasana", new Budget(income, expenses));
-//    }
-//
-//    @Test
-//    public void budjettiOnAlustettuOikein() {
-//        assertEquals(12123.4, user.getBudget().getTotalIncome(), 0.00001);
-//        assertEquals(11341.3, user.getBudget().getTotalExpenses(), 0.00001);
-//    }
-//}
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import budjetointisovellus.domain.Expense;
+import budjetointisovellus.domain.Income;
+import budjetointisovellus.domain.User;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author blesku
+ */
+public class UserTest {
+
+    User user;
+    List <Income> incomes = new ArrayList<>();
+    List <Expense> expenses = new ArrayList<>();
+    List<Income> incomesExpected = new ArrayList<>();
+    List<Expense> expensesExpected = new ArrayList<>();
+    List<Expense> expensesExpected1 = new ArrayList<>();
+    List<Income> incomesExpected1 = new ArrayList<>();
+
+
+    Expense expense;
+    Income income;
+    
+
+    @Before
+    public void setUp() {
+        this.user = new User("Jani", "salasana", incomes, expenses);
+        expense = new Expense("kalja", 10.6, user);
+        income = new Income("pullot", 2.6, user);
+        incomesExpected1.add(income);
+        expensesExpected1.add(expense);
+        
+
+    }
+
+    @Test
+    public void userOnAlustettuOikein() {
+        assertEquals("Jani", user.getName());
+        assertEquals("salasana", user.getPassword());
+        assertEquals(incomesExpected, user.getExpenses());
+        assertEquals(expensesExpected, user.getIncomes());
+    }
+    
+    
+    @Test
+    public void userAddListToimiiOikein() {
+        user.getExpenses().add(expense);
+        user.getIncomes().add(income);
+        assertEquals(incomesExpected1, user.getIncomes());
+        assertEquals(expensesExpected1, user.getExpenses());
+    }
+}
